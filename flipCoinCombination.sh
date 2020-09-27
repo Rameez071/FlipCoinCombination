9b6877d2f79fe1aa9d81a044900b4838b7415c44
Rameez071 <rameezraja405@gmail.com>
2020-09-27 20:21:28 +0530

    Add : triplet Combination added

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -1,43 +1,36 @@
 #!/bin/bash -x
 
-declare -A doubleDict
-for j in {1..100}
+read -p "how many times you want to flip the for triple combination ? " flips_for_triplet
+
+#dictionary declaretion
+
+declare -A TripletDictionary
+
+#Variables
+heads=0
+tails=0
+
+FLIP=$(($((RANDOM%10))%2))
+
+#loop for triplet combination of coins
+for i in `seq $flips_for_triplet`
 do
-	val1=$(((RANDOM%2)) );
-	val2=$(((RANDOM%2)) );
-	value=$val1$val2;
-
-	if [ "$value" == "HH" ]
-	then
-		hhCounter=$((hhCounter+1));
-	elif [ "$value" == "HT" ]
-	then
-		htCounter=$((htCounter+1));
-	elif [ "$value" == "TH" ]
-	then
-		thCounter=$((thCounter+1));
-	else
-		ttCounter=$((ttCounter+1));
-	fi
-
-	doubleDict[$j]=$value;
+        tripletFlip=$(($((RANDOM%8))+1))
+        case $tripletFlip in
+                1)TripletDictionary["key$i"]=`echo "HHH"`;;
+                2)TripletDictionary["key$i"]=`echo "HHT"`;;
+                3)TripletDictionary["key$i"]=`echo "HTH"`;;
+                4)TripletDictionary["key$i"]=`echo "THH"`;;
+		5)TripletDictionary["key$i"]=`echo "TTH"`;;
+                6)TripletDictionary["key$i"]=`echo "THT"`;; 
+		7)TripletDictionary["key$i"]=`echo "HTT"`;;
+		8)TripletDictionary["key$i"]=`echo "TTT"`;;
+		*)TripletDictionary["key$i"]=`echo "NA"`;;
+        esac
 done
 
-hhpercent1=`echo $hhCounter | awk '{print $1/100}'`;
-hhpercent2=`echo $hhpercent1 | awk '{print $1*100 }'`;
-
-ttPercent1=`echo $ttCounter | awk '{print $1/100}'`;
-ttPercent2=`echo $ttPercent1 | awk '{print $1*100}'`;
-
-thPercent1=`echo $thCounter | awk '{print $1/100}'`;
-thPercent2=`echo $thPercent1 | awk '{print $1*100}'`;
-
-htPercent1=`echo $htCounter | awk '{print $1/100}'`;
-htPercent2=`echo $htPercent1 | awk '{print $1*100}'`;
-
-echo "Percentage For Double Heads = "$hhpercent2"%";
-echo "Percentage For Double Tails = "$ttPercent2"%";
-echo "Percentage For HT = "$htPercent2"%";
-echo "Percentage for TH = "$thPercent2"%";
-
-echo "${doubleDict[@]}";
+#display triplet dictionary and its percentage
+echo "Dictionary to show Triplet Combination : ${TripletDictionary[@]}"
+triplet_count=${#TripletDictionary[@]}
+triplet_percent=`awk 'BEGIN{printf("%0.2f", '$triplet_count'*100)}'`
+echo "Triplet Percentage : $triplet_percent%"
