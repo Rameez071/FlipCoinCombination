c0500c48930dfd1e644d78f060d856c4a62451d0
Rameez071 <rameezraja405@gmail.com>
2020-09-27 19:43:17 +0530

    Add : Singlet percentage calculation code added

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -2,13 +2,31 @@
 
 echo "welcome to flip coin games"
 
-#Fliping Coin using random function
-toss=$(( RANDOM%2 ))
-if [ $toss -eq 1 ]
+toss=$((RANDOM%2))
+declare -A  singletDictionary
+
+if [ $toss -eq 0 ]
 then
-        echo "Head" $Head
-
+     echo  head
 else
-	echo "Tail" $Tail
-
+     echo tail
 fi
+for (( i=0; i<9; i++ ))
+do
+  toss=$((RANDOM%2))
+  if [ $toss -eq 0 ]
+  then
+       singletDictionary[head]=$((${singletDictionary[head]}+1))
+  else
+       singletDictionary[tail]=$((${singletDictionary[tail]}+1)) 
+  fi
+done
+
+echo "${singletDictionary[@]}"
+echo "${!singletDictionary[@]}"
+
+headper=$((${singletDictionary[head]}*100/i))
+tailper=$((${singletDictionary[tail]}*100/i))
+
+echo $tailper
+echo $headper
